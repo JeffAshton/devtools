94f388ec7693088e109ab4148dc129ec857f4926
Jeff Ashton <jeff.ashton@cantire.com>
2015-06-04 21:53:56 -0400

    Making check-status.sh smarter

--- check-status.sh
+++ check-status.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 for repo in $( ls -d */ ); do
-   echo "------------------- $repo -------------------"
    cd $repo
-   git status
+   echo "------------------- $repo -------------------"
+
+   status="$( git status )"
+
+   if [ -n "$( echo "$status" | grep 'git push' )" ]; then
+      echo "$status"
+   elif [ -n "$( echo "$status" | grep 'git reset' )" ]; then
+      echo "$status"
+   elif [ -n "$( echo "$status" | grep 'git add' )" ]; then
+      echo "$status"
+   fi
+
    cd ..
 done
 
