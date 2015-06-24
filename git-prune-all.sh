fa4c0c1b2c5eb7792b6cf9b8116fe5fcdd7361b5
Jeff Ashton <jeff.ashton@cantire.com>
2015-06-23 21:50:36 -0400

    Adding prune helper

---
+++ git-prune-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for repo in $( ls -d */ ); do
+   if [ $( ls -a $repo | grep ^.git$ ) ]; then
+       echo "------------------- $repo -------------------"
+       cd $repo
+       git remote prune origin
+       cd ..
+    fi
+done
+
