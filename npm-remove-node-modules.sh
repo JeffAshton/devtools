d5d7878303e6c3b6e1c2d73b95837e1ea2342bf8
Jeff Ashton <jeff.ashton@cantire.com>
2015-06-27 09:50:25 -0400

    Adding more helpers

---
+++ npm-remove-node-modules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for repo in $( ls -d */ ); do
+    if [ $( ls $repo | grep ^package.json$ ) ]; then
+       echo "------------------- $repo -------------------"
+       cd $repo
+       rm -f -r node_modules
+       cd ..
+    fi
+done
+
