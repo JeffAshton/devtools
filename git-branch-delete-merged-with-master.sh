42e842247abb5b0dea8cb27d44e64fa56716b2ca
Jeff Ashton <jeff.ashton@cantire.com>
2015-10-28 15:42:28 -0400

    Updating scripts

--- git-branch-delete-merged-with-master.sh
+++ git-branch-delete-merged-with-master.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
 
-git branch --merged origin/master | grep -v "\*" | grep -v master | grep -v dev | xargs -n 1 git branch -d
+MERGED_BRANCHES="$( git branch --merged origin/master | grep -v "\*" | grep -v master | grep -v dev )"
 
+if [ -n "$MERGED_BRANCHES" ]; then
+    echo "$MERGED_BRANCHES" | xargs -n 1 git branch -d
+fi
