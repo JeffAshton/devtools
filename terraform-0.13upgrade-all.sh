16157e24ba1a39a7ab7cdbd46e2e1bb005085512
Jeff Ashton <JeffAshton@users.noreply.github.com>
2021-01-08 18:16:55 -0500

    Skipping backend

--- terraform-0.13upgrade-all.sh
+++ terraform-0.13upgrade-all.sh
@@ -3,7 +3,7 @@ set -eu
 
 for child in $( ls -d */ ); do
     echo "Updating $child"
-    terraform-0.13 init -input=false -lock=false $child
+    terraform-0.13 init -backend=false -input=false -lock=false $child
     terraform-0.13 0.13upgrade -yes $child
 done
 
