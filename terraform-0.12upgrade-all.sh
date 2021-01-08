8600eaf27e39063569f4c24b81afe100c7da7b8b
Jeff Ashton <Jeff.Ashton@D2L.com>
2021-01-08 18:16:15 -0500

    No backend for upgrade

--- terraform-0.12upgrade-all.sh
+++ terraform-0.12upgrade-all.sh
@@ -3,7 +3,7 @@ set -eu
 
 for child in $( ls -d */ ); do
     echo "Updating $child"
-    terraform-0.12 init -input=false -lock=false $child
+    terraform-0.12 init -backend=false -input=false -lock=false $child
     terraform-0.12 0.12upgrade -yes $child
 done
 
